56ed2fafb105d99a54cd69c4bfc50f04b4c9ede8
Akashgowda209 <akashgowdas303@gmail.com>
2025-12-30 13:41:31 +0530

    Update product.js

--- product.js
+++ product.js
@@ -32,3 +32,47 @@ function orderProduct(productName) {
   window.location.href = "order_product.html";
 }
 
+
+const products = [
+  {
+    name: "Chicken Breast 500g",
+    price: 230
+  },
+  {
+    name: "Chicken Breast 1kg",
+    price: 440
+  },
+  {
+    name: "Eggs (30 pcs)",
+    price: 210
+  }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("productContainer");
+
+  products.forEach(p => {
+    container.innerHTML += `
+      <div class="product-card">
+        <h3>${p.name}</h3>
+        <p class="price">₹${p.price}</p>
+        <button onclick="orderProduct('${p.name}', ${p.price})">
+          Order
+        </button>
+      </div>
+    `;
+  });
+});
+
+function orderProduct(name, price) {
+  const msg =
+`Product Order
+Product: ${name}
+Price: ₹${price}`;
+
+  window.open(
+    "https://wa.me/918310405828?text=" + encodeURIComponent(msg),
+    "_blank"
+  );
+}
+
